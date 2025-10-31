afdd285d69a5c9bc10ea62bb6b7dc87c32d2348d
Filip Filipovic <102156199+RaptorFF@users.noreply.github.com>
2025-10-31 15:15:48 +0100

    Integrate StarRating and Test component for ratings
    Added Test component to manage movie ratings and integrated multiple StarRating components.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+//import "./index.css";
+//import App from "./App";
+import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating maxRating={10} color="blue" onSetRating={setMovieRating} />
+      <p>This movie have {movieRating} star rating.</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating maxRating={10} color="red" size={30} defaultRating={1} />
+    <Test />
   </React.StrictMode>
 );
